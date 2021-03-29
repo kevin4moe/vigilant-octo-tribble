35c388d480ea31f23bc25323a344b8763936f4d7
Kevin Pavon <kevin4moe@gmail.com>
2021-03-28 21:30:07 -0600

    add routes and ref to css in html

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <page-home />
+  <div class="container">
+    <router-view/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import PageHome from './components/PageHome.vue';
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld,
-    PageHome,
-  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,4 +1,22 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+import PageHome from '@/components/PageHome'
+import { createRouter, createWebHistory } from 'vue-router'
+
+const routes = [
+    {
+        path: '/',
+        name: 'Home',
+        component: PageHome
+    }
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+const forumApp = createApp(App);
+forumApp.use(router)
+forumApp.mount('#app')
