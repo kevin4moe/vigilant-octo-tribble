54af80e8395344eff745bf2cdf0031a124a1e0af
Kevin Pavon <kevin4moe@gmail.com>
2021-03-30 13:26:07 -0600

    Format config and changes related

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <template>
-  <nav>
-    <span>log in</span>
-    <span>sign up</span>
-  </nav>
-  <h1>LolIOs</h1>
-  <div class="container">
-    <router-view/>
-  </div>
+    <nav>
+        <span>log in</span>
+        <span>sign up</span>
+    </nav>
+    <h1>LolIOs</h1>
+    <div class="container">
+        <router-view />
+    </div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-}
+    export default {
+        name: "App",
+    };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,29 +1,29 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-import PageHome from '@/pages/PageHome'
-import ThreadShow from '@/pages/PageThreadShow'
-import { createRouter, createWebHistory } from 'vue-router'
+import PageHome from "@/pages/PageHome";
+import ThreadShow from "@/pages/PageThreadShow";
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
     {
-        path: '/',
-        name: 'Home',
-        component: PageHome
+        path: "/",
+        name: "Home",
+        component: PageHome,
     },
     {
-        path: '/thread/:id',
-        name: 'Thread Show',
+        path: "/thread/:id",
+        name: "Thread Show",
         component: ThreadShow,
         props: true,
-    }
-]
+    },
+];
 
 const router = createRouter({
     history: createWebHistory(),
-    routes
-})
+    routes,
+});
 
 const forumApp = createApp(App);
-forumApp.use(router)
-forumApp.mount('#app')
+forumApp.use(router);
+forumApp.mount("#app");
