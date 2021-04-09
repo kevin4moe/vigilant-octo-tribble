e43e7884bd0dfeae10150dfff619d320bb5041f8
Kevin Pavon <kevin4moe@gmail.com>
2021-04-08 23:39:01 -0500

    Deleted data files and ref

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -29,7 +29,6 @@
 <script>
 import FormVue from "@/components/Form.vue";
 import H2Default from "@/components/h2.vue";
-import sourceData from "@/dataL.json";
 export default {
     name: "PageHome",
     components: {
@@ -38,12 +37,6 @@ export default {
     },
     data() {
         return {
-            jsonData: {
-                threads: sourceData.threads,
-                posts: sourceData.posts,
-                users: sourceData.users,
-                favoritePosts: sourceData.stats.favoritePosts,
-            },
             formData: {
                 errors: [],
                 name: null,
@@ -55,12 +48,6 @@ export default {
         };
     },
     methods: {
-        postById(postId) {
-            return this.jsonData.posts.find((p) => p.id === postId);
-        },
-        userById(userId) {
-            return this.jsonData.users.find((p) => p.id === userId);
-        },
         saveData(data) {
             this.searchData = data;
         },
