e388bfb7d17fe5c3a75ce867d64ea7e0b7baa302
Kevin Pavon <kevin4moe@gmail.com>
2021-04-09 21:43:53 -0500

    Prevention of input errors

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex flex-col sm:flex-row mx-auto mb-1" id="app-form">
-        <div v-if="formData.errors.length">
-            <p v-for="(error, index) in formData.errors" :key="index">
+        <div>
+            <p v-for="(error, index) in errors" :key="index">
                 <b>{{ error }}</b>
             </p>
         </div>
@@ -19,9 +19,9 @@
             <div class="relative mx-1">
                 <ul
                     class="absolute flex flex-col w-full bg-white border border-pink-500 rounded-lg"
-                    v-show="tagsData.length > 0"
+                    v-show="searchTags.length > 0"
                 >
-                    <li v-for="(element, index) in tagsData" :key="index">
+                    <li v-for="(element, index) in searchTags" :key="index">
                         <a
                             class="block px-2 hover:bg-pink-500 focus:bg-pink-500 hover:text-white focus:text-white cursor-pointer"
                             tabindex="2"
@@ -44,15 +44,15 @@
                 class="mx-1 mt-1 p-2 w-auto bg-pink-500 active:bg-white border border-pink-500 rounded-full font-semibold text-white"
                 type="submit"
                 value="Update"
-                @click="$emit('search', formData.tags.arrayData)"
+                @click="checkForm()"
             />
         </div>
     </div>
     <div class="flex flex-row">
         <span
             class="mr-1 px-3 py-1 bg-pink-100 hover:bg-pink-300 border border-pink-500 rounded-full cursor-pointer"
-            v-for="(element, index) in formData.tags.arrayData"
-            @click="formData.tags.arrayData.splice(index, 1)"
+            v-for="(element, index) in tagList"
+            @click="tagList.splice(index, 1)"
             :key="index"
             >{{ element }}</span
         >
@@ -60,49 +60,44 @@
 </template>
 <script>
 export default {
+    name: "FormComponent",
     emits: ["search"],
     data() {
         return {
-            formData: {
-                errors: [],
-                name: null,
-                tags: {
-                    isTrue: false,
-                    arrayData: [],
-                },
-                inputs: false,
-            },
-            tagsData: [],
+            errors: [],
+            tagList: [],
+            searchTags: [],
         };
     },
     methods: {
-        checkForm(e) {
-            if (this.formData.tags) {
-                return true;
-            }
-            this.formData.errors = [];
-            if (!this.formData.tags) {
-                this.formData.errors.push(
-                    "No olvides escribir lo que quieres buscar."
-                );
-            }
-
-            e.preventDefault();
+        checkForm() {
+            this.$emit("search", this.tagList);
         },
         searchTagsMatches(search) {
             fetch(
                 `https://danbooru.donmai.us/tags.json?search[name_matches]=${search}*&search[order]=count&limit=5`
             )
                 .then((response) => response.json())
-                .then((data) => (this.tagsData = data))
+                .then((data) => (this.searchTags = data))
                 .catch((err) => console.log(err));
         },
         targetClean(target) {
-            target
-                ? this.formData.tags.arrayData.push(target)
-                : (this.formData.tags.arrayData = []);
             this.$refs.mainInputElement.value = "";
-            this.tagsData = [];
+            this.searchTags = [];
+            this.errors = [];
+            target ? this.errorControl("double", target) : (this.tagList = []);
+        },
+        errorControl(action, value) {
+            if (this.tagList.length > 1) {
+                this.errors.push("Solo se permiten 2 tags.");
+                return false;
+            }
+            if (action === "double") {
+                var isDouble = this.tagList.find((item) => value === item);
+                !isDouble
+                    ? this.tagList.push(value)
+                    : this.errors.push("Tag is already exists.");
+            }
         },
     },
 };
