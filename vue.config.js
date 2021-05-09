2bf74b485f2016c1e779d61efe063df370c225df
Kevin Pavon <kevin4moe@gmail.com>
2021-05-09 00:44:46 -0500

    Some fix to pwa

--- vue.config.js
+++ vue.config.js
@@ -6,4 +6,26 @@ module.exports = {
             return args;
         });
     },
+    pwa: {
+        name: "LolIOs",
+        short_name: "LolIOs",
+        iconPaths: {
+            favicon512: "favicon/android-chrome-512x512.png",
+            favicon192: "favicon/android-chrome-192x192.png",
+            favicon32: "favicon/favicon-32x32.png",
+            favicon16: "favicon/favicon-16x16.png",
+            appleTouchIcon: "favicon/apple-touch-icon.png",
+            maskIcon: null,
+            msTileImage: null,
+        },
+        start_url: "/",
+        scope: "/",
+        theme_color: "#ec4899",
+        background_color: "#ffffff",
+        display: "standalone",
+        related_applications: [],
+        prefer_related_applications: false,
+
+        workboxPluginMode: "InjectManifest",
+    },
 };
