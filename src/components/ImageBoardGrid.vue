bec550bf0d70ef3d59a6bf5cda0314a4585e5655
Kevin Pavon <kevin4moe@gmail.com>
2021-05-09 00:07:48 -0500

    Image view and title

--- src/components/ImageBoardGrid.vue
+++ src/components/ImageBoardGrid.vue
@@ -2,6 +2,7 @@
     <main class="grid grid-cols-3 sm:grid-cols-5 md:grid-cols-6 gap-1 my-4">
         <figure class="mx-auto" v-for="post in postsObject" :key="post.id">
             <img
+                v-if="post.preview_file_url"
                 :src="post.preview_file_url"
                 :title="`Character(s):
             ${post.tag_string_character}`"
---
+++ src/components/MainTitle.vue
@@ -0,0 +1,14 @@
+<template>
+    <h2
+        class="my-8 border-b-2 border-pink-100 text-pink-500 text-center text-3xl font-sans"
+    >
+        About {{ Intl.NumberFormat().format(many) }} results [{{ rating }}][{{
+            page
+        }}]
+    </h2>
+</template>
+<script>
+export default {
+    props: ["many", "rating", "page"],
+};
+</script>
